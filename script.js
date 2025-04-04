425af495fedebe30ea198631b367fd4782cf3c21
danielmova <dataxf@gmail.com>
2025-04-04 17:45:35 -0500

    Added images and fixing Css problems For the last couple of day I work in the sprites for this game. The only one that I take from the internet is the background. I change some Css to visibility issues.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const playGameBtnJs = document.querySelector("#playGameBtn");
 
 const roundPara = document.querySelector(".optionsPara");
 
+const odinPixelArtJs = document.querySelector("#odinPixelArt");
+
 let humanScore = 0;
 
 let computerScore = 0;
@@ -24,34 +26,41 @@ function playRound(button_id) {
     let winnerRound = "";
     
     if (humanChoice === computerChoice) {
+        odinPixelArtJs.src = "images/OdinIdle.gif";
         winnerRound = `Tie!\n ${humanChoice} vs ${computerChoice}`;
     }
     else if (humanChoice === "rock") {
         if (computerChoice === "paper") {
+            odinPixelArtJs.src = "images/OdinWinRound.gif";
             winnerRound = `Odin Win this round!!!\n${humanChoice} vs ${computerChoice}`;
             computerScore++;
         } 
         else if (computerChoice === "scissors") {
+            odinPixelArtJs.src = "images/OdinLooseRound.gif";
             winnerRound = `You Win this round...\n${humanChoice} vs ${computerChoice}`;
             humanScore++;
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
+            odinPixelArtJs.src = "images/OdinWinRound.gif";
             winnerRound = `Odin Win this round!!!\n${humanChoice} vs ${computerChoice}`;
             computerScore++;
         }
         else if (computerChoice === "rock") {
+            odinPixelArtJs.src = "images/OdinLooseRound.gif";
             winnerRound = `You Win this round...\n${humanChoice} vs ${computerChoice}`;
             humanScore++;
         }
     }
     else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
+            odinPixelArtJs.src = "images/OdinWinRound.gif";
             winnerRound = `Odin Win this round!!!\n${humanChoice} vs ${computerChoice}`;
             computerScore++;
         }
         else if (computerChoice === "paper") {
+            odinPixelArtJs.src = "images/OdinLooseRound.gif";
             winnerRound = `You Win this round...\n${humanChoice} vs ${computerChoice}`;
             humanScore++;
         }
@@ -70,14 +79,17 @@ function playRound(button_id) {
         let winnerGame = "";
 
         if (humanScore == computerScore){
+            odinPixelArtJs.src = "images/OdinIdle.gif";
             winnerGame = `Tie!\n You: ${humanScore} vs Odin: ${computerScore}\nYou are a formidable opponent...`;
         }
     
         else if (humanScore < computerScore){
+            odinPixelArtJs.src = "images/OdinWinGame.gif";
             winnerGame = `You Lose!!!\n You: ${humanScore} vs Odin: ${computerScore}\nOdin sees everything...`;
         }
     
         else if (humanScore > computerScore){
+            odinPixelArtJs.src = "images/OdinLooseGame.gif";
             winnerGame = `Congratulations!!!\n You: ${humanScore} vs Odin: ${computerScore}\nYou Win!!!\n...\nFor now...`;
         }
         
@@ -102,6 +114,7 @@ function playGame(){
 
     resoultsPara.innerText = "";
     roundPara.innerText = "Round 1 \n Chose your weapon...";
+    odinPixelArtJs.src = "images/OdinIdle.gif";
 
     playGameBtnJs.style.display = "none";
     document.querySelector(".optionsContainer").style.display = "flex";
